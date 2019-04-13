7cb64c395252edcbb67b77a63983cdf9ab42c24b
林将平 <2329shohei@gmail.com>
2019-04-13 17:19:00 +0900

    first commit

--- src/Game.kt
+++ src/Game.kt
@@ -6,23 +6,20 @@ fun main(args: Array<String>) {
 
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        "is in excellent condition!"
-    } else if (healthPoints in 90..99) {
-        "has a few scratches."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
             "has some minor wounds."
         }
-    } else if (healthPoints in 15..74) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!"
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
     }
 
-    println(name + " " + healthStatus)
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
 }
