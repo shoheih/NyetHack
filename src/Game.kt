7f97000ac37538723ff208be26f7271f4233fa45
林将平 <2329shohei@gmail.com>
2019-04-17 07:56:31 +0900

    first commit

--- src/Game.kt
+++ src/Game.kt
@@ -7,6 +7,14 @@ fun main(args: Array<String>) {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
 
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
@@ -18,8 +26,5 @@ fun main(args: Array<String>) {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
     }
-
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
+    return healthStatus
 }
