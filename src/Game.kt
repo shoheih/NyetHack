ffa2bb9c6c7ab6554c5fe4ea3cd4eb8b7f3714b8
林将平 <2329shohei@gmail.com>
2019-04-23 14:56:59 +0900

    add default parameter

--- src/Game.kt
+++ src/Game.kt
@@ -12,7 +12,7 @@ fun main(args: Array<String>) {
     // Player status
     printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 
-    castFireball(5)
+    castFireball()
 }
 
 private fun printPlayerStatus(
@@ -49,6 +49,6 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     return healthStatus
 }
 
-private fun castFireball(numFireballs: Int) {
+private fun castFireball(numFireballs: Int = 2) {
     println("A glass of Fireball springs into existence.（x$numFireballs）")
 }
